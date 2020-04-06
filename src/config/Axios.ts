79b39efb4d518c17ae45874aedc557004dce2ae2
Jorge Gomez <jorge6242@gmail.com>
2020-04-06 00:07:36 -0400

    axios config

--- src/config/Axios.ts
+++ src/config/Axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 //'http://portal.api.com
 
 const AXIOS = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'http://portal.api.com',
   headers: {
     'Content-Type': 'application/json',
     'Partners-Application': 'portal'
