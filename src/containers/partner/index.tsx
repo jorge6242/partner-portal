917bbb6ff1638cd922d308b7b215cceda883f1f2
Jorge Gomez <jorge6242@gmail.com>
2020-04-14 16:20:33 -0400

    change

--- src/containers/partner/index.tsx
+++ src/containers/partner/index.tsx
@@ -33,7 +33,7 @@ const columns: PersonColumn[] = [
     },
     {
       id: "isPartner",
-      label: "parentesto",
+      label: "Parentesco",
       minWidth: 30,
       align: "right",
       component: (value: any) => {
