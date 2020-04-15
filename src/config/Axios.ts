6426d9b59f5709cc367c4ffeb41bf3d92befce10
Jorge Gomez <jorge6242@gmail.com>
2020-04-15 14:48:56 -0400

    pack 1

--- src/config/Axios.ts
+++ src/config/Axios.ts
@@ -3,9 +3,11 @@ import axios from 'axios';
 //'http://portal.api.com
 //'http://localhost:8000
 //'http://localhost/api/portalsocios
+//http://190.216.224.53/api/portalsocios/
+//http://192.168.0.251/api/portalsocios/
 
 const AXIOS = axios.create({
-  baseURL: 'http://portal.api.com',
+  baseURL: 'http://190.216.224.53/api/portalsocios',
   headers: {
     'Content-Type': 'application/json',
     'Partners-Application': 'portal'
