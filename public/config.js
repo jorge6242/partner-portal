226977bba1bd1d021ede5827030639f15d527c3c
Jorge Gomez <jorge6242@gmail.com>
2020-05-01 19:35:47 -0400

    axios url config

---
+++ public/config.js
@@ -0,0 +1,1 @@
+var BASE_URL = "http://portal.api.com";
--- src/config/Axios.js
+++ src/config/Axios.js
@@ -14,8 +14,10 @@ import axios from 'axios';
 
 //192.168.0.252:9001
 
+const BASE_URL = window.BASE_URL;
+console.log('window.BASE_URL ', window.BASE_URL);
 const AXIOS = axios.create({
-  baseURL: 'http://192.168.0.252:9001',
+  baseURL: BASE_URL,
   headers: {
     'Content-Type': 'application/json',
     'Partners-Application': 'portal'
