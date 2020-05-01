b5e98d32eee894fdaaeb505619411e3b264bfb5c
Jorge Gomez <jorge6242@gmail.com>
2020-05-01 17:19:04 -0400

    validating paypal button

--- src/components/Paypal/index.tsx
+++ src/components/Paypal/index.tsx
@@ -15,6 +15,7 @@ interface ComponentProps {
 
 const Paypal: FunctionComponent<ComponentProps> = ({ invoiceId, description, customId, amount, amountDetail }) => {
     const dispatch = useDispatch();
+    const client = "Ab8frqGsF4rlmjIH9mS9kTdaGo2-vLh-v0PK5G1ZxeKBSTbAkygWF3eRCPYydHRtQBGlRJyLPDY4v5Aw";
     return (
         <Grid container spacing={2}>
             <Grid item xs={12}>
@@ -25,56 +26,54 @@ const Paypal: FunctionComponent<ComponentProps> = ({ invoiceId, description, cus
                 </Grid>
             </Grid>
             <Grid item xs={12}>
-                <PayPalButton
-                    onButtonReady={(e:any) => {
-                        console.log('ready ', e);
-                    }}
-                    createOrder={(data: any, actions: any) => {
-                        console.log('createOrder');
-                        return actions.order.create({
-                            purchase_units: [{
-                                description,
-                                custom_id: customId,
-                                invoice_id: `F-${invoiceId}`,
-                                amount: {
-                                    currency_code: "USD",
-                                    value: amount
-                                }
-                            }],
-                        });
-                    }}
-                    onApprove={(data: any, actions: any) => {
-                        // Capture the funds from the transaction
-                        dispatch(updateModal({
-                            payload: {
-                              isLoader: true,
-                            }
-                          }));
-                        return actions.order.capture().then( async (details: any) => {
-                            // Show a success message to your buyer
-                            // alert("Transaction completed by " + details.payer.name.given_name);
-                            console.log('data ', data);
-                            console.log('details ', details);
-                            // OPTIONAL: Call your server to save the transaction
-                            const body = {
-                                order: data.orderID,
-                                invoice: `F-${invoiceId}`,
-                                amount
-                            };
-                            await dispatch(setOrder(body));
-                            dispatch(getUnpaidInvoices());
-                            dispatch(getReportedPayments());
-                            dispatch(updateModal({
-                                payload: {
-                                  isLoader: false,
-                                }
-                              }));
-                        });
-                    }}
-                    options={{
-                        clientId: "Ab8frqGsF4rlmjIH9mS9kTdaGo2-vLh-v0PK5G1ZxeKBSTbAkygWF3eRCPYydHRtQBGlRJyLPDY4v5Aw"
-                    }}
-                />
+                {
+                    client && (
+                        <PayPalButton
+                            createOrder={(data: any, actions: any) => {
+                                return actions.order.create({
+                                    purchase_units: [{
+                                        description,
+                                        custom_id: customId,
+                                        invoice_id: `F-${invoiceId}`,
+                                        amount: {
+                                            currency_code: "USD",
+                                            value: amount
+                                        }
+                                    }],
+                                });
+                            }}
+                            onApprove={(data: any, actions: any) => {
+                                // Capture the funds from the transaction
+                                dispatch(updateModal({
+                                    payload: {
+                                        isLoader: true,
+                                    }
+                                }));
+                                return actions.order.capture().then(async (details: any) => {
+                                    // Show a success message to your buyer
+                                    // alert("Transaction completed by " + details.payer.name.given_name);
+                                    // OPTIONAL: Call your server to save the transaction
+                                    const body = {
+                                        order: data.orderID,
+                                        invoice: `F-${invoiceId}`,
+                                        amount
+                                    };
+                                    await dispatch(setOrder(body));
+                                    dispatch(getUnpaidInvoices());
+                                    dispatch(getReportedPayments());
+                                    dispatch(updateModal({
+                                        payload: {
+                                            isLoader: false,
+                                        }
+                                    }));
+                                });
+                            }}
+                            options={{
+                                clientId: client
+                            }}
+                        />
+                    )
+                }
             </Grid>
         </Grid>
     );
