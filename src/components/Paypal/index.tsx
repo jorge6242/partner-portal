f85e6991a9e8421ae4c86d14ecd9a656e3528a95
Jorge Gomez <jorge6242@gmail.com>
2020-05-05 08:35:00 -0400

    remove invoiceid test

--- src/components/Paypal/index.tsx
+++ src/components/Paypal/index.tsx
@@ -32,7 +32,7 @@ const Paypal: FunctionComponent<ComponentProps> = ({ invoiceId, description, cus
                         return actions.order.create({
                             purchase_units: [{
                                 description,
-                                custom_id: `CC-${customId}`,
+                                custom_id: customId,
                                 invoice_id: invoiceId,
                                 amount: {
                                     currency_code: "USD",
@@ -54,7 +54,7 @@ const Paypal: FunctionComponent<ComponentProps> = ({ invoiceId, description, cus
                             // OPTIONAL: Call your server to save the transaction
                             const body = {
                                 order: data.orderID,
-                                invoice: `CC-${customId}`,
+                                invoice: customId,
                                 amount
                             };
                             await dispatch(setOrder(body));
