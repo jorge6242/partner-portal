a1187e1e8286e9cc2276f9960a0b0970d10323c2
Jorge <jorge6242@gmail.com>
2020-07-11 13:32:01 -0400

    lower case in search local array function

--- src/containers/pending-invoices/filter.js
+++ src/containers/pending-invoices/filter.js
@@ -1,6 +1,10 @@
+
+
 export function search(user){
-    return Object.keys(this).every((key) => user[key].match(this[key]));
+    return Object.keys(this).every((key) => user[key].toLowerCase().match(this[key].toLowerCase()));
 }
+
+//return Object.keys(this).every((key) => user[key] === this[key]);
 
             // if(form.status !== '' && form.fact_num !== '' && form.descrip !== '') {
             //     data = data.filter((element: any) => element.status === form.status && element.fact_num === form.fact_num && element.descrip === form.descrip);
